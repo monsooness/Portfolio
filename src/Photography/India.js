be36d833f35f6dc6bbfe59058f280e082b20c771
Monsoon <nepzsoon@gmail.com>
2018-10-06 23:49:28 -0700

    Succefully load photos for india and nepal

--- src/Photography/India.js
+++ src/Photography/India.js
@@ -1,20 +1,16 @@
 import React, { Component } from 'react';
 
-
 function importAll(r) {
-      let images = {};
-      r.keys().map((item, index) => { images[item.replace('./', '')] = r(item); });
-      return images;
+  let images = {};
+  r.keys().map((item, index) => { images[item.replace('./', '')] = r(item); });
+  return images;
 }
     
-  
 const images = importAll(require.context('./Photos/India/', false, /\.(png|jpe?g|svg)$/));
-const x = [1,2]
-console.log(images);
 
 const India = (props) => (
   <div className="photoGrid">
-    { Object.keys(images).map( (t) => <img src={images[t]}/>  ) }
+    { Object.keys(images).map( (t) => <img className="photos" src={images[t]}/>  ) }
   </div>
 )
 
--- src/Photography/Nepal.js
+++ src/Photography/Nepal.js
@@ -1,22 +1,17 @@
 import React, { Component } from 'react';
 
-class Nepal extends Component {
-
-  constructor(props) {
-    super(props)
-  }
-  
-  render() {
-    const items = [...Array(100)].map((val, i) => `Item ${i}`);
-    return (
-      <div className="photoGrid">
-       Nepal
-       <ul>
-        {items.map((item, i) => (<li key={`item_${i}`}>{ item }</li>))}
-      </ul>
-      </div>
-    );
-  }
+function importAll(r) {
+  let images = {};
+  r.keys().map((item, index) => { images[item.replace('./', '')] = r(item); });
+  return images;
 }
+    
+const images = importAll(require.context('./Photos/Nepal/', false, /\.(png|jpe?g|svg)$/));
+
+const Nepal = (props) => (
+  <div className="photoGrid">
+    { Object.keys(images).map( (t) => <img className="photos" src={images[t]}/>  ) }
+  </div>
+)
 
 export default Nepal
