27d017391d572a5e4997b2369555f276e792b781
Monsoon <nepzsoon@gmail.com>
2018-10-16 18:03:38 -0700

    Implement static about me

--- src/Components/Aboutme/Aboutme.js
+++ src/Components/Aboutme/Aboutme.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { withRouter} from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
 
 import headshot from './Photos/headshot.JPG'
 
@@ -14,8 +14,16 @@ class Aboutme extends Component {
         <div className="intro">
           Creating > Consuming
         </div>
+
         <div className="about-container">
-          <h1>Hello</h1>
+            <h2>MERN stack developer. </h2>
+            <p> I strike for modularity on the front end </p>
+            <p> And scalability on the back end </p>
+            <p> Eager to learn and build full-stack projects </p>
+            <p> With newer technologies and larger datasets.</p>
+            <br></br>
+            <p>You can contact me at:</p>
+            <p>nepzsoon@gmail.com</p>
         </div>
       </div>
     )
