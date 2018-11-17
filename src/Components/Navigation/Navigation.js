e97de25ef73824dcd7d1876fbb738f8a4b76d71d
Monsoon <nepzsoon@gmail.com>
2018-11-17 01:23:37 -0800

    Implement resume overlapping fix and minor problems

--- src/Components/Navigation/Navigation.js
+++ src/Components/Navigation/Navigation.js
@@ -8,12 +8,12 @@ const Navigation = () => {
     <div className="navbar">
       <div className="left">
         <NavLink exact to="/" className="inactive" activeClassName="active">Photography</NavLink>
-        <NavLink to="/mathart" className="inactive" activeClassName="active">Math Art</NavLink>
+        <NavLink to="/Mathart" className="inactive" activeClassName="active">Math Art</NavLink>
       </div>
 
       <div className="right">
-        <NavLink to="/resume" className="inactive" activeClassName="active">Resume</NavLink>
-        <NavLink to="/aboutme" className="inactive" activeClassName="active">About Me</NavLink> 
+        <NavLink to="/Resume" className="inactive" activeClassName="active">Resume</NavLink>
+        <NavLink to="/Aboutme" className="inactive" activeClassName="active">About Me</NavLink> 
       </div>
     </div>
   );
